bb0cb5b25449b13b6db1ccf153b0ab00676634d5
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 19:34:41 -0700

    ♻️ Use function of wstools

--- src/tilt_match.R
+++ src/tilt_match.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(wstools)
 
-liverpool <- read_team_stats("/workdir/data/Liverpool.csv")
-pases_totales <- liverpool |>
-  filter(grepl("Premier", Competition)) |>
-  group_by(Date) |>
-  summarize(total_pases = sum(Passes_to_final_third_accurate))
+liverpool <- read_team_stats("/workdir/data/Liverpool.csv") |>
+  filter_premier_league()
 
 liverpool <- liverpool |>
-  left_join(pases_totales, by = "Date") |>
-  mutate(tilt = 100 * Passes_to_final_third_accurate / total_pases)
+  add_tilt()
+liverpool |>
+  filter(Team == "Liverpool") |>
+  select(1,2,7:8,109:111)
+
