d7a02848d424fe41a24479996f089cf1b0002b1f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-26 14:53:34 -0700

    ♻️ Use for loop

--- src/tilt_match.R
+++ src/tilt_match.R
@@ -1,46 +1,22 @@
 library(tidyverse)
+library(jsonlite)
 library(wstools)
 
 names <- c("Liverpool", "Chelsea", "Manchester City", "Tottenham Hotspur", "Arsenal", "Brighton")
-liverpool <- read_team_stats("/workdir/data/Liverpool.csv") |>
-  filter_premier_league()
 
-liverpool <- liverpool |>
-  add_tilt() |>
-  filter(Team == "Liverpool") |>
-  select(1,2,7:8,109:111)
-
-chelsea <- read_team_stats("/workdir/data/Chelsea.csv") |>
-  filter_premier_league()
-
-chelsea <- chelsea |>
-  add_tilt() |>
-  filter(Team == "Chelsea")
-
-city <- read_team_stats("/workdir/data/Manchester_City.csv") |>
-  filter_premier_league()
-
-city <- city |>
-  add_tilt() |>
-  filter(Team == "Manchester City")
-
-arsenal <- read_team_stats("/workdir/data/Arsenal.csv") |>
-  filter_premier_league()
-
-arsenal <- arsenal |>
-  add_tilt() |>
-  filter(Team == "Arsenal")
-
-tottenham <- read_team_stats("/workdir/data/Tottenham_Hotspur.csv") |>
-  filter_premier_league()
-
-tottenham <- tottenham |>
-  add_tilt() |>
-  filter(Team == "Tottenham Hotspur")
-
-brighton <- read_team_stats("/workdir/data/Brighton.csv") |>
-  filter_premier_league()
-
-brighton <- brighton |>
-  add_tilt() |>
-  filter(Team == "Brighton")
+all_team_stats <- list()
+for (team in names) {
+  team_name <- str_replace(team, " ", "_")
+  path <- glue::glue("/workdir/data/{team_name}.csv")
+  team_stats <- read_team_stats(path) |>
+    filter_premier_league() |>
+    add_tilt() |>
+    filter(Team == team)
+  all_team_stats[[team]] <- team_stats
+}
+all_team_tilt <- list()
+for (team in names(all_team_stats)) {
+  all_team_tilt[[team]] <- quantile(all_team_stats[[team]]$tilt)
+}
+ListJSON <- toJSON(all_team_tilt, pretty = TRUE, auto_unbox = TRUE)
+write(ListJSON, "quantiles_tilt.json")
