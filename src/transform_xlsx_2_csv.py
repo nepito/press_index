d71f1eff8859032a87a9627b5f224ca193cf7b64
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-04-29 15:46:09 -0700

    🪲 Fix bug
    The names did not fit.

--- src/transform_xlsx_2_csv.py
+++ src/transform_xlsx_2_csv.py
@@ -6,8 +6,8 @@ files_to_change = [file.replace(" ","_") for file in all_files_xlsx]
 
 
 def return_transformation_command(xlsx_files: str, file_to_change):
-    return f"in2csv '/workdir/data/serie_a/{xlsx_files}' > /workdir/data/{file_to_change}.csv"
+    return f"in2csv '/workdir/data/serie_a/Team Stats {xlsx_files}.xlsx' > /workdir/data/{file_to_change}.csv"
 
 
-for old_file, new_file in zip(all_files_in_data, files_to_change):
+for old_file, new_file in zip(all_files_xlsx, files_to_change):
     os.system(return_transformation_command(old_file, new_file))
