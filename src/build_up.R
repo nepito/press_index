73cb5e2f1133451c309942ec9056b8d0927c3e4f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-04-29 14:33:32 -0700

    🎨 Format with Styler

--- src/build_up.R
+++ src/build_up.R
@@ -7,7 +7,7 @@ league <- "263"
 teams_names_by_league <- list(
   "39" = c("Liverpool", "Chelsea", "Manchester City", "Tottenham Hotspur", "Arsenal", "Brighton", "Crystal Palace", "Aston Villa", "Brentford", "Everton", "Fulham", "Leeds United", "Leicester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Southampton", "West Ham United", "Wolverhampton Wanderers", "Bournemouth"),
   "262" = c("América", "Atlas", "Atlético de San Luis", "Club Tijuana", "Cruz Azul", "Guadalajara", "Juárez", "León", "Mazatlán", "Monterrey", "Necaxa", "Pachuca", "Puebla", "Pumas UNAM", "Querétaro", "Santos Laguna", "Tigres UANL", "Toluca"),
-  "263" = c("Alebrijes de Oaxaca", "Cancún", "Dorados", "Raya2", "Universidad Guadalajara", "Atlante", "Celaya","Durango","Tapatío","Venados","Atlético Morelia","Cimarrones de Sonora", "Mineros de Zacatecas","Tepatitlán de Morelos","CA La Paz","Correcaminos UAT","Pumas Tabasco","Tlaxcala")
+  "263" = c("Alebrijes de Oaxaca", "Cancún", "Dorados", "Raya2", "Universidad Guadalajara", "Atlante", "Celaya", "Durango", "Tapatío", "Venados", "Atlético Morelia", "Cimarrones de Sonora", "Mineros de Zacatecas", "Tepatitlán de Morelos", "CA La Paz", "Correcaminos UAT", "Pumas Tabasco", "Tlaxcala")
 )
 names <- teams_names_by_league[[league]]
 all_team_stats <- list()
@@ -16,12 +16,12 @@ for (team in names) {
   team_name <- str_replace_all(team, " ", "_")
   path <- glue::glue("/workdir/data/{team_name}.csv")
   team_stats <- read_team_stats(path) |>
-#     filter_premier_league() |>
+    #     filter_premier_league() |>
     add_tilt() |>
     filter(Team == team)
   all_team_stats[[team]] <- team_stats
   team_stats <- read_team_stats(path) |>
-#     filter_premier_league() |>
+    #     filter_premier_league() |>
     filter(Team != team)
   all_rivals_team[[team]] <- team_stats
 }
