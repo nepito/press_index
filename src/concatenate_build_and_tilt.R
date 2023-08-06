9821c4642d646791cbfb91de524268b0042c4a5c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-06 00:15:32 -0700

    🚧 Use id league

--- src/concatenate_build_and_tilt.R
+++ src/concatenate_build_and_tilt.R
@@ -3,9 +3,9 @@ library("comprehenr")
 library(jsonlite)
 
 league <- "135"
-tilts <- fromJSON("quantiles_tilt_135.json")
-ppda <- read_csv("build_up_and_ppda_135.csv", show_col_types = FALSE)
+tilts <- fromJSON(glue::glue("quantiles_tilt_{league}.json"))
+ppda <- read_csv(glue::glue("build_up_and_ppda_{league}.csv"), show_col_types = FALSE)
 teams <- ppda$team
 tilt <- to_vec(for (team in teams) tilts[[team]][3])
 ppda$tilt <- tilt
-ppda |> write_csv("xG_build-up_ppda_tilt_135.csv")
+ppda |> write_csv(glue::glue("xG_build-up_ppda_tilt_{league}.csv"))
