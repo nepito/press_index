b775a1a9330880a888fbde1fe2e45f58dd40d70f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 17:22:41 -0800

    ✅ Pass test

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -1,3 +1,10 @@
 return_one <- function() {
   return(1)
 }
+
+normalize_delta <- function(raw_data) {
+  min_d <- min(raw_data$delta, na.rm = TRUE)
+  max_d <- max(raw_data$delta, na.rm = TRUE)
+  diff_d <- max_d - min_d
+  return(raw_data |> dplyr::mutate(delta_n = 1 + (delta - min_d) / diff_d))
+}
