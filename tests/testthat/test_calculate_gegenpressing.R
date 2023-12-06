d1b2bc749d7f8eb3e08773fc77791aa7f6010f97
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 20:05:31 -0800

    ✅ Update  `test_calculate_ggpi()`

--- tests/testthat/test_calculate_gegenpressing.R
+++ tests/testthat/test_calculate_gegenpressing.R
@@ -1,5 +1,5 @@
 describe("Calculate Gegenpressing", {
-  it("delta", {
+  it("ggpi", {
     raw <- tibble::tibble(
       delta = c(1, 2, 3, 4, 5),
       tempo = c(1, 2, 3, 4, 5),
@@ -7,8 +7,8 @@ describe("Calculate Gegenpressing", {
       build_up_disruption = c(1, 2, 3, 4, 5),
       ppda = c(1, 2, 3, 4, 5)
     )
-    expected <- c(1, 1.2, 1.3, 1.4, 1.5)
+    expected <- c(1, 1.191, 1.435, 1.708, 2.000)
     obtained <- calculate_ggpi(raw)
-    expect_equal(expected, obtained$ggpi)
+    expect_equal(expected, obtained$ggpi, tolerance = 1e-3)
   })
 })
