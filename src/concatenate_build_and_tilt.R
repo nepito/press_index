bde92b2ade83c6a54679a7ca3ab1f3bfccd8c5b2
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 22:29:25 -0800

    🚧 Add delta and tempo

--- src/concatenate_build_and_tilt.R
+++ src/concatenate_build_and_tilt.R
@@ -2,10 +2,16 @@ library(tidyverse)
 library("comprehenr")
 library(jsonlite)
 
-league <- "135"
+league <- "78"
 tilts <- fromJSON(glue::glue("quantiles_tilt_{league}.json"))
+deltas <- fromJSON(glue::glue("quantiles_delta_{league}.json"))
+tempos <- fromJSON(glue::glue("quantiles_tempo_{league}.json"))
 ppda <- read_csv(glue::glue("build_up_and_ppda_{league}.csv"), show_col_types = FALSE)
 teams <- ppda$team
 tilt <- to_vec(for (team in teams) tilts[[team]][3])
+delta <- to_vec(for (team in teams) deltas[[team]][1])
+tempo <- to_vec(for (team in teams) tempos[[team]][1])
 ppda$tilt <- tilt
-ppda |> write_csv(glue::glue("xG_build-up_ppda_tilt_{league}.csv"))
+ppda$delta <- delta
+ppda$tempo <- tempo
+ppda |> write_csv(glue::glue("xG_build-up_ppda_tilt_{league}_2023-24.csv"))
