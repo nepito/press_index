02037542ef1033a6984a71599378d5a2e4a2f2d4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 17:48:22 -0800

    ♻️ Extract `normalize_index()`

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -10,8 +10,14 @@ normalize_delta <- function(raw_data) {
 }
 
 normalize_ppda <- function(raw_data) {
-  min_d <- min(raw_data$ppda, na.rm = TRUE)
-  max_d <- max(raw_data$ppda, na.rm = TRUE)
+  return(normalize_index(raw_data, "ppda"))
+}
+
+normalize_index <- function(raw_data, col_name) {
+  min_d <- min(raw_data[[col_name]], na.rm = TRUE)
+  max_d <- max(raw_data[[col_name]], na.rm = TRUE)
   diff_d <- max_d - min_d
-  return(raw_data |> dplyr::mutate(ppda_n = 1 + (ppda - min_d) / diff_d))
+  new_name <- glue::glue("{col_name}_n")
+  raw_data[[new_name]] <- 1 + (raw_data[[col_name]] - min_d) / diff_d
+  return(raw_data)
 }
