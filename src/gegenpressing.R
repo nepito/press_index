89c2d140552c4f205ff7f8481dc3c65b45a1c823
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 22:27:08 -0800

    🚧 Add all files

--- src/gegenpressing.R
+++ src/gegenpressing.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(pression)
-teams <- read_csv("/workdir/xG_build-up_ppda_tilt_39_2023-24.csv", show_col_types = FALSE)
+paths <- list.files("/workdir/", pattern = "_2023-24\\.csv$")
+teams <- read_csv(paths, show_col_types = FALSE)
 gegenpressing <- teams |>
   pression::calculate_ggpi()
