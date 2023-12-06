1360d2b086f022ea92214f293a0897250880d8d9
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 17:50:50 -0800

    ♻️ Rename function `normalize_index()`

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -3,14 +3,14 @@ return_one <- function() {
 }
 
 normalize_delta <- function(raw_data) {
-  return(normalize_index(raw_data, "delta"))
+  return(.normalize_index(raw_data, "delta"))
 }
 
 normalize_ppda <- function(raw_data) {
-  return(normalize_index(raw_data, "ppda"))
+  return(.normalize_index(raw_data, "ppda"))
 }
 
-normalize_index <- function(raw_data, col_name) {
+.normalize_index <- function(raw_data, col_name) {
   min_d <- min(raw_data[[col_name]], na.rm = TRUE)
   max_d <- max(raw_data[[col_name]], na.rm = TRUE)
   diff_d <- max_d - min_d
