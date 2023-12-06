b0ea8b152700b894fc15c78028038e97686bc999
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 17:25:08 -0800

    🛑🧪 Test `normalize_ppda()`

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -22,4 +22,10 @@ describe("Normalize variable", {
     obtained <- normalize_delta(raw)
     expect_equal(expected, obtained)
   })
+  it("ppda", {
+    raw <- tibble::tibble(ppda = c(1, 2, 3, 4, 5))
+    expected <- tibble::tibble(ppda = c(1, 2, 3, 4, 5), ppda_n = c(1, 1.25, 1.5, 1.75, 2))
+    obtained <- normalize_ppda(raw)
+    expect_equal(expected, obtained)
+  })
 })
