9685b5f550556c2a30e55453b037e809ec8b63d9
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-05 20:05:57 -0800

    ✅ Pass test

---
+++ R/calculate_gegenpressibg.R
@@ -0,0 +1,9 @@
+calculate_ggpi <- function(raw_data) {
+  data_with_ggpri <- raw_data |>
+    normalize_delta() |>
+    normalize_ppda() |>
+    normalize_bdp() |>
+    calculate_offensive_transition() |>
+    normalize_offe_tran() |>
+    dplyr::mutate(ggpi = (build_up_disruption_n * offe_tran_n) / ppda_n)
+}
