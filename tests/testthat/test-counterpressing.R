cefcc0fb40822d7965d7c4c866fa17ee90385870
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-08 20:17:15 -0800

    🛑🧪 Test `Counterpressing$set_raw_data()`

--- tests/testthat/test-counterpressing.R
+++ tests/testthat/test-counterpressing.R
@@ -1,5 +1,7 @@
 describe("Counterpressing", {
   it("Init", {
-    Counterpressing$new()
+    frankfurt <- readr::read_csv("/workdir/tests/data/Eintracht_Frankfurt.csv", show_col_types = FALSE)
+    coun_press <- Counterpressing$new()
+    coun_press$set_raw_data(frankfurt)
   })
 })
