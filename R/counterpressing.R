c8c040338884e7cc862819fc38cb014ec11757fc
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-08 21:08:32 -0800

    ♻️ Refactor

--- R/counterpressing.R
+++ R/counterpressing.R
@@ -7,13 +7,16 @@ Counterpressing <- R6::R6Class("Counterpressing", list(
   set_raw_data = function(raw_data) {
     self$raw_data <- raw_data
     private$get_matches()
-    self$losses_recovery_rivals <- self$raw_data |>
-      dplyr::filter(Team != "Eintracht Frankfurt")
+    private$set_losses_recovery_rivals()
   }
 ),
 private = list(
   get_matches = function() {
     self$matches <- self$raw_data$Match |> unique()
+  },
+  set_losses_recovery_rivals = function() {
+    self$losses_recovery_rivals <- self$raw_data |>
+      dplyr::filter(Team != "Eintracht Frankfurt")
   }
 )
 )
