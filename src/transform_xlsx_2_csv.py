2edd9ae1517012e0ab1ec7c902c1b65bc9a2d67a
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-08 21:10:41 -0800

    🚧 Save csv in the same directory

--- src/transform_xlsx_2_csv.py
+++ src/transform_xlsx_2_csv.py
@@ -1,13 +1,13 @@
 import os
 
-folder_path = "serieb"
+folder_path = "premier"
 all_files_in_data = [file for _, _, file in os.walk(f"/workdir/data/{folder_path}")][0]
 all_files_xlsx = [file.split(".")[0].split("Team Stats ")[1] for file in all_files_in_data if file.split(".")[1] == "xlsx"]
 files_to_change = [file.replace(" ","_") for file in all_files_xlsx]
 
 
 def return_transformation_command(xlsx_files: str, file_to_change):
-    return f"in2csv '/workdir/data/{folder_path}/Team Stats {xlsx_files}.xlsx' > /workdir/data/{file_to_change}.csv"
+    return f"in2csv '/workdir/data/{folder_path}/Team Stats {xlsx_files}.xlsx' > /workdir/data/{folder_path}/{file_to_change}.csv"
 
 
 for old_file, new_file in zip(all_files_xlsx, files_to_change):
