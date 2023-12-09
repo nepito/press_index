4a940600b1a0e8a82cb840fa4c100ed234110343
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-08 20:37:12 -0800

    ✅ Pass tests

--- R/counterpressing.R
+++ R/counterpressing.R
@@ -1,8 +1,10 @@
 Counterpressing <- R6::R6Class("Counterpressing", list(
   raw_data = NULL,
+  matches = NULL,
   initialize = function(path, team_name) {
   },
   set_raw_data = function(raw_data) {
     self$raw_data <- raw_data
+    self$matches <- self$raw_data$Match |> unique()
   }
 ))
