105af76ff354f3fe5b5b8ef230bae0694da7904c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-12-08 20:28:26 -0800

    ✅ Pass tests

--- tests/testthat/test-counterpressing.R
+++ tests/testthat/test-counterpressing.R
@@ -3,5 +3,8 @@ describe("Counterpressing", {
     frankfurt <- readr::read_csv("/workdir/tests/data/Eintracht_Frankfurt.csv", show_col_types = FALSE)
     coun_press <- Counterpressing$new()
     coun_press$set_raw_data(frankfurt)
+    expected_n_row <- 12
+    obtained_n_row <- nrow(coun_press$raw_data)
+    expect_equal(obtained_n_row, expected_n_row)
   })
 })
