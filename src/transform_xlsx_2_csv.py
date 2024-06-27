154874039ccae462e204b74c25461a1b7a4843dd
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-26 21:05:47 -0700

    🚧 Chage to CLI

--- src/transform_xlsx_2_csv.py
+++ src/transform_xlsx_2_csv.py
@@ -1,14 +1,23 @@
 import os
+import typer
 
-folder_path = "premier"
-all_files_in_data = [file for _, _, file in os.walk(f"/workdir/data/{folder_path}")][0]
-all_files_xlsx = [file.split(".")[0].split("Team Stats ")[1] for file in all_files_in_data if file.split(".")[1] == "xlsx"]
-files_to_change = [file.replace(" ","_") for file in all_files_xlsx]
+def obtain_all_files_xlsx(folder_path: str) -> list:
+    all_files_in_data = [file for _, _, file in os.walk(f"/workdir/data/{folder_path}")][0]
+    return [file.split(".")[0].split("Team Stats ")[1] for file in all_files_in_data if file.split(".")[1] == "xlsx"]
 
 
-def return_transformation_command(xlsx_files: str, file_to_change):
+def return_transformation_command(folder_path: str, xlsx_files: str, file_to_change):
     return f"in2csv '/workdir/data/{folder_path}/Team Stats {xlsx_files}.xlsx' > /workdir/data/{folder_path}/{file_to_change}.csv"
 
+tx2c = typer.Typer(help="Awesome CLI user manager.")
 
-for old_file, new_file in zip(all_files_xlsx, files_to_change):
-    os.system(return_transformation_command(old_file, new_file))
+
+@tx2c.command()
+def main(folder_path: str = "serie_a_2023-24"):
+    all_files_xlsx: list = obtain_all_files_xlsx(folder_path)
+    files_to_change = [file.replace(" ","_") for file in all_files_xlsx]
+    for old_file, new_file in zip(all_files_xlsx, files_to_change):
+        os.system(return_transformation_command(folder_path, old_file, new_file))
+
+if __name__ == "__main__":
+    tx2c()
