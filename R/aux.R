96aea7ae589726fc615b30e0f77d3d363944ffbd
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-06-29 12:33:30 -0700

    ♻️ Extract `.remove_prefixes()`

--- R/aux.R
+++ R/aux.R
@@ -4,8 +4,12 @@ obtain_files_names <- function(path_directory) {
 }
 
 .clean_files_name <- function(files_name) {
-  lists_names <- files_name |> stringr::str_split("Stats ")
-  names_with_xlsx <- comprehenr::to_vec(for (team in lists_names) team[2])
+  names_with_xlsx <- .remove_prefixes(files_name)
   lists_cleaned_names <- names_with_xlsx |> stringr::str_split(".xlsx")
   comprehenr::to_vec(for (team in lists_cleaned_names) team[1])
+}
+
+.remove_prefixes <- function(files_name) {
+  lists_names <- files_name |> stringr::str_split("Stats ")
+  names_with_xlsx <- comprehenr::to_vec(for (team in lists_names) team[2])
 }
