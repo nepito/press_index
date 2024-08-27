f36d11cfc2a2d6c753351c144a21f280784b9040
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-08-26 21:35:58 -0700

    🚧  Add ggpi index

--- src/gegenpressing.R
+++ src/gegenpressing.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(pression)
-paths <- list.files("/workdir/", pattern = "_2023-24\\.csv$")
+paths <- "xG_build-up_ppda_tilt_78_2024_2023-24.csv"
 teams <- read_csv(paths, show_col_types = FALSE)
 gegenpressing <- teams |>
-  pression::calculate_ggpi()
+  pression::calculate_ggpi() |>
+  select(-c(offe_tran_n, build_up_disruption_n, ppda_n, delta_n)) |>
+  write_csv("pression_index_bundesliga.csv")
