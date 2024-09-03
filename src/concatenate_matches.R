45c69c98b2c95c76460575a494ebdd3ac52c42a7
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-02 20:50:50 -0700

    🚧 Improve the use of `list.files()`

--- src/concatenate_matches.R
+++ src/concatenate_matches.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(jsonlite)
-dp <- fromJSON("data/ligaMX_2023-24/datapackage.json")
+dp <- fromJSON("data/datapackage.json")
 names_from_dp <- dp$resources$schema$fields[[1]]$name
-files <- list.files("/workdir/data/ligaMX_2023-24", pattern = ".csv$")
-path_files <- glue::glue("/workdir/data/ligaMX_2023-24/{files}")
-data <- read_csv(path_files, show_col_types = FALSE, col_names = names_from_dp, skip = 1) |>
+root_path <- "/workdir/data/ligaMX_2023-24/2024-25"
+files <- list.files(root_path, pattern = ".csv$", full.names = TRUE)
+data <- read_csv(files, show_col_types = FALSE, col_names = names_from_dp, skip = 1) |>
   filter(!is.na(Match), Competition == "Mexico. Liga MX") |>
-  distinct(Date, Match, Team, .keep_all = TRUE)
+  distinct(Date, Match, Team, .keep_all = TRUE) |>
+  write_csv(glue::glue("{root_path}/wyscout_matches.csv"))
