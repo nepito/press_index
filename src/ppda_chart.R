6702c63be7d52386cf780771233314927c753695
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-11 21:09:09 -0700

    ♻️ Extract `add_wheel_index()`

--- src/ppda_chart.R
+++ src/ppda_chart.R
@@ -9,25 +9,27 @@ liga_mx <- data |>
   distinct(date, match, team, .keep_all = TRUE) |>
   filter(stringr::str_detect(competition,"Mexico" ))
 
+add_wheel_index <- function(league_data, left_align = FALSE) {
+  how_align <- "right"
+  if (left_align) {how_align <- "left"}
+  league_data_with_wheel_index <- league_data |>
+    mutate(
+      central_p = passes*100/crosses,
+      ppda_mean = zoo::rollapply(ppda, width = 4, mean, fill = NA, align=how_align),
+      tempo_mean = zoo::rollapply(match_tempo, width = 4, mean, fill = NA, align=how_align),
+      possession_mean = zoo::rollapply(possession_percent, width = 4, mean, fill = NA, align=how_align),
+      central_p_mean = zoo::rollapply(central_p, width = 4, mean, fill = NA, align=how_align)
+    )
+  return(league_data_with_wheel_index)
+}
+
 random_xolos <- liga_mx |>
   filter(team == "Club Tijuana") |>
   sample_frac(replace = TRUE) |>
-  mutate(
-    central_p = passes*100/crosses,
-    ppda_mean = zoo::rollapply(ppda, width = 4, mean, fill = NA, align="left"),
-    tempo_mean = zoo::rollapply(match_tempo, width = 4, mean, fill = NA, align="left"),
-    possession_mean = zoo::rollapply(possession_percent, width = 4, mean, fill = NA, align="left"),
-    central_p_mean = zoo::rollapply(central_p, width = 4, mean, fill = NA, align="left")
-  ) |>
+  add_wheel_index(left_align = TRUE) |>
   select(1:3,ppda_mean, tempo_mean, central_p_mean, possession_mean)
 
 chart_xolos <- liga_mx |>
   filter(team == "Club Tijuana") |>
-  mutate(
-    central_p = passes*100/crosses,
-    ppda_mean = zoo::rollapply(ppda, width = 4, mean, fill = NA, align="right"),
-    tempo_mean = zoo::rollapply(match_tempo, width = 4, mean, fill = NA, align="right"),
-    possession_mean = zoo::rollapply(possession_percent, width = 4, mean, fill = NA, align="right"),
-    central_p_mean = zoo::rollapply(central_p, width = 4, mean, fill = NA, align="right")
-  ) |>
+  add_wheel_index() |>
   select(1:3,ppda_mean, tempo_mean, central_p_mean, possession_mean)
