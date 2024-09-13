4f5ae1b11fc18eed19295d40741e2f1997bc6ea6
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-12 17:29:40 -0700

    ✅ Test `add_wheel_index()` other example

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -6,4 +6,10 @@ describe("add_wheel_index()", {
     expected <- c(50, 46.44, 50, 50.255)
     expect_equal(obtained[seq(n_rows,n_rows-3, -1)], expected)
   })
+  it("ppda_mean", {
+    obtained <- data |> add_wheel_index() |> dplyr::pull(ppda_mean)
+    n_rows <- length(obtained)
+    expected <- c(12.4, 12.4, 9.8, 9.4)
+    expect_equal(obtained[seq(n_rows,n_rows-3, -1)], expected, tolerance=1e-2)
+  })
 })
