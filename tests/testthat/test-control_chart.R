2961cc392436d14461a618360ca960b143b66e32
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-12 17:34:39 -0700

    ✅ Test `add_wheel_index()`: central_p_mean left_align

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -18,4 +18,10 @@ describe("add_wheel_index()", {
     expected <- c(4756, 2434, 2374, 2245)
     expect_equal(obtained[seq(n_rows,n_rows-3, -1)], expected, tolerance=1e-2)
   })
+  it("central_p_mean: left", {
+    obtained <- data |> add_wheel_index(left_align = TRUE) |> dplyr::pull(central_p_mean)
+    n_rows <- length(obtained)
+    expected <- c(2943, 2947, 2772, 3572)
+    expect_equal(obtained[1:4], expected, tolerance=1e-2)
+  })
 })
