a734bd3938a23378fe90b2152735bc8eaeb5509b
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-09-12 18:25:05 -0700

    ♻️ Extract `data_with_wheel`

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -1,24 +1,22 @@
 describe("add_wheel_index()", {
   data <- readr::read_csv("/workdir/tests/data/tijuana.csv", show_col_types = FALSE)
+  data_with_wheel <- data |> add_wheel_index()
   it("possession_mean", {
-    obtained <- data |>
-      add_wheel_index() |>
+    obtained <- data_with_wheel |>
       dplyr::pull(possession_mean)
     n_rows <- length(obtained)
     expected <- c(50, 46.44, 50, 50.255)
     expect_equal(obtained[seq(n_rows, n_rows - 3, -1)], expected)
   })
   it("ppda_mean", {
-    obtained <- data |>
-      add_wheel_index() |>
+    obtained <- data_with_wheel |>
       dplyr::pull(ppda_mean)
     n_rows <- length(obtained)
     expected <- c(12.4, 12.4, 9.8, 9.4)
     expect_equal(obtained[seq(n_rows, n_rows - 3, -1)], expected, tolerance = 1e-2)
   })
   it("central_p_mean", {
-    obtained <- data |>
-      add_wheel_index() |>
+    obtained <- data_with_wheel |>
       dplyr::pull(central_p_mean)
     n_rows <- length(obtained)
     expected <- c(4756, 2434, 2374, 2245)
